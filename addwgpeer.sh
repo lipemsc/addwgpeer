3ed749243f89563058d02d99de91d49e68184f02
Filipe <60903547+lipemsc@users.noreply.github.com>
2025-02-07 12:14:40 +0000

    Create addwgpeer.sh

---
+++ addwgpeer.sh
@@ -0,0 +1,23 @@
+PRIVKEY=$(wg genkey)
+
+PUBKEY=$(echo $PRIVKEY | wg pubkey)
+
+SERVERPUBKEY=$(wg show $1 public-key)
+
+#echo $PRIVKEY
+#echo $PUBKEY
+
+wg set $1 peer $PUBKEY
+
+echo "
+[Interface]
+Address = $2
+DNS = 1.1.1.1
+PrivateKey = $PRIVKEY
+
+[Peer]
+PublicKey = $SERVERPUBKEY
+AllowedIPs = 0.0.0.0/0
+Endpoint = $3
+" | qrencode -t utf8
+
